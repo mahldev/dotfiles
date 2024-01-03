314c43580da3186cc0e308440f9ef565a0eee3c6
mahl <robertolopezt75@gmail.com>
2024-01-03 20:51:54 +0100

    fixing neovim dotfiles

--- noevim/nvim/lua/plugins/harpoon.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "ThePrimeagen/harpoon",
-  lazy = false,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  branch = "harpoon2",
-  config = true,
-}
--- noevim/nvim/lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      layout_strategy = "vertical",
-      layout_config = { preview_cutoff = 6 },
-    },
-  },
-}
