d68bf04598dda045aa1055ee0682e914a9f8d9b8
mahl <robertolopezt75@gmail.com>
2024-01-08 00:17:38 +0100

    add wallpapers directory

--- rofi/tmux/run.sh
+++ rofi/tmux/run.sh
@@ -7,22 +7,22 @@ active=" (active)"
 
 # Active sessions.
 readarray -t active_sessions < <(~/.config/tmux/utils/workspace_sessions.sh)
-for ((i=0; i<${#active_sessions[@]}; i++)); do
-  active_sessions[i]="$prefix${active_sessions[i]}$active"
+for ((i = 0; i < ${#active_sessions[@]}; i++)); do
+	active_sessions[i]="$prefix${active_sessions[i]}$active"
 done
 
 # Get configs.
 files=("$dir"/*.sh)
-for (( i=0; i<${#files[@]}; i++ )); do
-  file="${files[i]}"
-  file="${file##*/}"
-  session_name="${file%.sh}"
-  if tmux has-session -t "$session_name" >/dev/null 2>&1; then
-    state_name="$session_name$active"
-  else
-    state_name="$session_name"
-  fi
-  files[i]="${prefix}${state_name}"
+for ((i = 0; i < ${#files[@]}; i++)); do
+	file="${files[i]}"
+	file="${file##*/}"
+	session_name="${file%.sh}"
+	if tmux has-session -t "$session_name" >/dev/null 2>&1; then
+		state_name="$session_name$active"
+	else
+		state_name="$session_name"
+	fi
+	files[i]="${prefix}${state_name}"
 done
 
 # Combine sessions.
@@ -30,21 +30,20 @@ all_sessions=("${active_sessions[@]}")
 all_sessions+=("${files[@]}")
 
 # Display with rofi.
-selected_file=$(printf '%s\n' "${all_sessions[@]}" | \
-    rofi \
-        -config "$HOME/.config/rofi/tmux/style.rasi" \
-        -dmenu \
-        -p "󱓞  Tmux" \
-        -no-custom)
+selected_file=$(printf '%s\n' "${all_sessions[@]}" |
+	rofi \
+		-config "$HOME/.config/rofi/tmux/style.rasi" \
+		-dmenu \
+		-p "󱓞  Tmux" \
+		-no-custom)
 
 # Start selected session.
 if [[ -n $selected_file ]]; then
-    script="${selected_file#$prefix}"
-    session_name="${script%$active}"
-    if tmux has-session -t "$session_name" >/dev/null 2>&1; then
-        kitty --class "tmux-$session_name" --execute tmux attach -t "$session_name"
-    else
-        kitty --class "tmux-$session_name" --execute "$HOME/.config/tmux/sessions/$session_name.sh"
-    fi
+	script="${selected_file#$prefix}"
+	session_name="${script%$active}"
+	if tmux has-session -t "$session_name" >/dev/null 2>&1; then
+		kitty --class "tmux-$session_name" --execute tmux attach -t "$session_name"
+	else
+		kitty --class "tmux-$session_name" --execute "$HOME/.config/tmux/sessions/$session_name.sh"
+	fi
 fi
-
